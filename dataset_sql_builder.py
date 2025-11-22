09328e37561246b2b6d5c4e94909135b4b77b7a0
KrisNathan <kristopher.nathanael@binus.ac.id>
2025-11-22 19:48:13 +0700

    added scripts to apply migration and generate sql from dataset csv

---
+++ dataset_sql_builder.py
@@ -0,0 +1,177 @@
+import csv
+import math
+from datetime import datetime
+from decimal import Decimal
+from typing import Iterator, Optional
+
+from mnemonic import Mnemonic
+from pydantic import BaseModel, Field, FiniteFloat, ValidationError
+
+
+def sql_escape(s: str) -> str:
+    return s.replace("'", "''")
+
+
+# Example CSV
+# Invoice,StockCode,Description,Quantity,InvoiceDate,Price,Customer ID,Country
+# 489434,85048,15CM CHRISTMAS GLASS BALL 20 LIGHTS,12,2009-12-01 07:45:00,6.95,13085.0,United Kingdom
+
+
+class DataRow(BaseModel):
+    invoice: str
+    stock_code: str
+    description: Optional[str]
+    quantity: int
+    invoice_date: datetime
+    price: Decimal
+    country: str
+    customer_id: Optional[str]
+
+
+def read_typed_csv(filepath: str) -> Iterator[DataRow]:
+    """Reads a CSV file and yields typed NamedTuple objects."""
+    with open(filepath, "r", newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            parsed_dt = datetime.strptime(
+                row["InvoiceDate"].strip(), "%Y-%m-%d %H:%M:%S"
+            )
+            yield DataRow(
+                invoice=row["Invoice"].upper(),
+                stock_code=row["StockCode"].upper(),
+                description=None if row["Description"] == "" else row["Description"],
+                quantity=abs(int(row["Quantity"])),
+                invoice_date=parsed_dt,
+                price=Decimal(row["Price"]),
+                country=row["Country"],
+                customer_id=None
+                if row["Customer ID"] == ""
+                else row["Customer ID"].upper(),
+            )
+
+
+class Customer(BaseModel):
+    customer_id: str
+    customer_name: str
+    customer_country: str
+
+
+class Item(BaseModel):
+    stock_code: str
+    description: str
+    price: Decimal = Field(gt=Decimal("0"))
+    inventory_quantity: int = Field(gt=0)
+
+
+class Invoice(BaseModel):
+    invoice_id: str
+    invoice_date: datetime
+    customer_id: str
+    total_price: Decimal
+
+
+class InvoiceItem(BaseModel):
+    invoice_id: str
+    item_stock_code: str
+    quantity: int
+
+
+def main():
+    customers: dict[str, Customer] = {}
+    items: dict[str, Item] = {}
+    invoices: dict[str, Invoice] = {}
+    invoice_items: dict[str, InvoiceItem] = {}
+
+    mnemo = Mnemonic("english")
+
+    for row in read_typed_csv("online_retail_data.csv"):
+        # print(row)
+        if row.customer_id not in customers and row.customer_id:
+            try:
+                m = mnemo.generate(strength=128)
+                cname = " ".join(m.split()[:2])
+                customer = Customer(
+                    customer_id=row.customer_id,
+                    customer_name=cname,
+                    customer_country=row.country,
+                )
+                customers[row.customer_id] = customer
+            except Exception as e:
+                # print(f"{row}: {e}")
+                pass
+
+        if row.stock_code not in items and row.description:
+            try:
+                item = Item(
+                    stock_code=row.stock_code,
+                    description=row.description,
+                    price=row.price,
+                    inventory_quantity=10,  # no idea
+                )
+                items[row.stock_code] = item
+            except ValidationError as e:
+                pass
+
+        if row.invoice not in invoices and row.customer_id:
+            try:
+                invoice = Invoice(
+                    invoice_id=row.invoice,
+                    invoice_date=row.invoice_date,
+                    customer_id=row.customer_id,
+                    total_price=row.price,
+                )
+                invoices[row.invoice] = invoice
+            except Exception as e:
+                # print(f"ERROR: {row}\n{e}")
+                pass
+
+        if (
+            row.invoice
+            and row.stock_code
+            and row.invoice in invoices
+            and row.stock_code in items
+        ):
+            invoice_item_key = f"{row.invoice}-{row.stock_code}"
+            if invoice_item_key not in invoice_items:
+                try:
+                    invoice_item = InvoiceItem(
+                        invoice_id=row.invoice,
+                        item_stock_code=row.stock_code,
+                        quantity=row.quantity,
+                    )
+                    invoice_items[invoice_item_key] = invoice_item
+                except Exception as e:
+                    pass
+
+    # Write SQL to dataset.sql
+    with open("dataset.sql", "w") as f:
+        f.write("INSERT INTO Customer VALUES\n")
+        values = [
+            f"('{sql_escape(customer.customer_id)}', '{sql_escape(customer.customer_name)}', '{sql_escape(customer.customer_country)}')"
+            for customer in customers.values()
+        ]
+        f.write(",\n".join(values) + ";\n\n")
+
+        f.write("INSERT INTO Item VALUES\n")
+        values = [
+            f"('{sql_escape(item.stock_code)}', '{sql_escape(item.description)}', {item.price}, {item.inventory_quantity})"
+            for item in items.values()
+        ]
+        f.write(",\n".join(values) + ";\n\n")
+        f.write("INSERT INTO Invoice VALUES\n")
+        values = [
+            f"('{sql_escape(invoice.invoice_id)}', '{invoice.invoice_date.strftime('%Y-%m-%d %H:%M:%S')}', '{sql_escape(invoice.customer_id)}', {invoice.total_price})"
+            for invoice in invoices.values()
+        ]
+        f.write(",\n".join(values) + ";\n\n")
+
+        f.write("INSERT INTO InvoiceItem VALUES\n")
+        values = [
+            f"('{sql_escape(invoice_item.invoice_id)}', '{sql_escape(invoice_item.item_stock_code)}', {invoice_item.quantity})"
+            for invoice_item in invoice_items.values()
+        ]
+        f.write(",\n".join(values) + ";\n\n")
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,5 +1,58 @@
+import random
+
+import mysql.connector
+from pydantic import BaseModel, Field, field_validator
+
+
+class DB:
+    def __init__(self):
+        self.mydb = mysql.connector.connect(
+            host="localhost", port=3306, user="root", password="example"
+        )
+
+    def cursor(self):
+        return self.mydb.cursor()
+
+    def commit(self):
+        self.mydb.commit()
+
+    def close(self):
+        self.mydb.close()
+
+
+def apply_migration(db: DB):
+    cursor = db.cursor()
+
+    # execute migration.sql
+    with open("migration.sql", "r") as f:
+        sql = f.read()
+        statements = [s.strip() for s in sql.split(";") if s.strip()]
+        for statement in statements:
+            cursor.execute(statement)
+
+    db.commit()
+
+
+def populate_data(db: DB):
+    cursor = db.cursor()
+
+    # execute migration.sql
+    with open("dataset.sql", "r") as f:
+        sql = f.read()
+        statements = [s.strip() for s in sql.split(";") if s.strip()]
+        for statement in statements:
+            cursor.execute(statement)
+
+    db.commit()
+
 def main():
-    print("Hello from db-project-tokopee!")
+    db = DB()
+
+    apply_migration(db)
+
+    populate_data(db)
+
+    db.close()
 
 
 if __name__ == "__main__":
