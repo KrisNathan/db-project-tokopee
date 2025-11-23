8bf8059f4d1ed3703b5dc509d94600482a8feb10
danielson <danielsonhong@gmail.com>
2025-11-23 20:30:50 +0700

    Refactor trigger procedure test: improve comment clarity for inventory operations

--- sql/trigger_procedure_test.sql
+++ sql/trigger_procedure_test.sql
@@ -1,26 +1,26 @@
 -- Check initial state
 SELECT StockCode, InventoryQuantity FROM Item WHERE StockCode = 'NEW001';
 
--- TEST 1: Sell 5 items (Trigger should reduce Inventory by 5)
+-- sell 5 items, should reduce Inventory by 5
 INSERT INTO Invoice (InvoiceId, InvoiceDate, CustomerId, TotalPrice) 
 VALUES ('TEST_AUTO_01', NOW(), '12346', 0);
 INSERT INTO InvoiceItem (InvoiceId, ItemStockCode, Quantity) 
 VALUES ('TEST_AUTO_01', 'NEW001', 5);
 
--- Verify Inventory (Should be 5 less)
+-- verify inventory, should be 5 less
 SELECT StockCode, InventoryQuantity FROM Item WHERE StockCode = 'NEW001';
 
--- TEST 2: Partial Return/Update (Change Qty from 5 to 2) -> Return 3 items
+-- partial return change quantity from 5 to 2 -> Return 3 items
 UPDATE InvoiceItem SET Quantity = 2 WHERE InvoiceId = 'TEST_AUTO_01';
 
--- Verify Inventory (Should increase by 3)
+-- verify inventory, should increase 3
 SELECT StockCode, InventoryQuantity FROM Item WHERE StockCode = 'NEW001';
 
--- TEST 3: Full Return (Delete Row) -> Return remaining 2 items
+-- full return -> return remaining 2 items
 DELETE FROM InvoiceItem WHERE InvoiceId = 'TEST_AUTO_01';
 
--- Verify Inventory (Should be back to original)
+-- verify inventory, should be back to original
 SELECT StockCode, InventoryQuantity FROM Item WHERE StockCode = 'NEW001';
 
--- TEST 4: Run Stored Procedure
+-- run stored procedure
 CALL GetCustomerInvoiceHistory('12682');
