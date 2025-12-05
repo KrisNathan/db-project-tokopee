052b6e5bf1be9f2874026034d4cc793bd56e48b9
olga <florenciaolga2022@gmail.com>
2025-12-06 01:09:49 +0700

    Update trigger_procedure_test.sql

--- sql/trigger_procedure_test.sql
+++ sql/trigger_procedure_test.sql
@@ -8,19 +8,19 @@ INSERT INTO InvoiceItem (InvoiceId, ItemStockCode, Quantity)
 VALUES ('TEST_AUTO_01', 'NEW001', 5);
 
 -- verify inventory, should be 5 less
-SELECT StockCode, InventoryQuantity FROM Item WHERE StockCode = 'NEW001';
+SELECT ItemStockCode, InventoryQuantity FROM Item WHERE ItemStockCode = 'NEW001';
 
 -- partial return change quantity from 5 to 2 -> Return 3 items
 UPDATE InvoiceItem SET Quantity = 2 WHERE InvoiceId = 'TEST_AUTO_01';
 
 -- verify inventory, should increase 3
-SELECT StockCode, InventoryQuantity FROM Item WHERE StockCode = 'NEW001';
+SELECT ItemStockCode, InventoryQuantity FROM Item WHERE ItemStockCode = 'NEW001';
 
 -- full return -> return remaining 2 items
 DELETE FROM InvoiceItem WHERE InvoiceId = 'TEST_AUTO_01';
 
 -- verify inventory, should be back to original
-SELECT StockCode, InventoryQuantity FROM Item WHERE StockCode = 'NEW001';
+SELECT ItemStockCode, InventoryQuantity FROM Item WHERE ItemStockCode = 'NEW001';
 
 -- run stored procedure
 CALL GetCustomerInvoiceHistory('12682');
